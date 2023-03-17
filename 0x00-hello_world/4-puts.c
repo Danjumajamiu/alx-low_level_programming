d428727ecc87a3948a9494fe886e6e0340dd9d36
Danjumajamiu <jamiudanju1@gmail.com>
2023-03-17 13:08:47 -0700

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * the main func is the entry point
+ *
+ * Return: Always 0 means code run is a Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
