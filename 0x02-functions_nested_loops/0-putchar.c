9c9e546106e82cc1631b6e4aac814614515c9b18
Danjumajamiu <jamiudanju1@gmail.com>
2023-03-21 15:59:03 -0700

    added 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int first, end;
-
-	end = sizeof(str) / sizeof(int);
-	for (first = 0; first < end; first++)
-	{
-		_putchar(str[first]);
-	}
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchrar(114};
 	_putchar('\n');
+
 	return (0);
 }
 
