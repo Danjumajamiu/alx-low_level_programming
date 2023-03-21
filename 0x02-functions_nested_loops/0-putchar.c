c49aaf8f10149d99b7fc6bd3ba47aa5f706cd421
Danjumajamiu <jamiudanju1@gmail.com>
2023-03-21 09:46:13 -0700

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Displays _putchar as a message.
+ *
+ * Return: Value is 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int first, end;
+
+	end = sizeof(str) / sizeof(int);
+	for (first = 0; first < end; first++)
+	{
+		_putchar(str[first]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ * _putchar - prints the string _putchar
+ * 
+ * Return: Success 1.
+ * Error -1.
+ */
+int _putchar(char _putchar)
+{
+	return (write(1, &_putchar, 1));
+}
