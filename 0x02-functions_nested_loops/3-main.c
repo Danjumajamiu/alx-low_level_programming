daadabd2eeb66289b836a96aef7d201c544ddd7d
Danjumajamiu <jamiudanju1@gmail.com>
2023-03-21 19:01:23 -0700

    updated

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    int ri;
+    int r;
 
-    ri = _islower('H');
-    _putchar(ri + '0');
-    ri = _islower('o');
-    _putchar(ri + '0');
-    ri = _islower(108);
-    _putchar(ri + '0');
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
     _putchar('\n');
     return (0);
 }
