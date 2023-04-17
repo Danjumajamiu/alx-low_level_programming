c685653d82b2888c0e5f62d72fe21ed577c52852
Danjumajamiu <jamiudanju1@gmail.com>
2023-04-17 07:30:47 -0700

    updated

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * Macro: 3-function_like_macro.h
+ * Detail: Header file that defines a function-like macro ABS(x)
+ *       that calculated the absolute value of a num x.
+ */
+
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#endif
